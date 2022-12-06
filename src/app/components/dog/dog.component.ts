8f97059e1db0aa85de5b0c2b0ecc9a72af823720
Grazielle127 <grazigrazielle454@gmail.com>
2022-12-05 23:49:51 -0300

    criando o componente dog

---
+++ src/app/components/dog/dog.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Dog } from 'src/app/interfaces/dog';
+import { DogApiServiceService } from 'src/app/services/dog-api-service.service';
+
+@Component({
+  selector: 'app-dog',
+  templateUrl: './dog.component.html',
+  styleUrls: ['./dog.component.css']
+})
+export class DogComponent implements OnInit {
+
+  dog!: Dog;
+
+  constructor(private dogApiService: DogApiServiceService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  
+}
